8a90684ab751c248369c69af854c42de07bab7d6
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-12-16 00:23:13 +0700

    feat: add timer creation

---
+++ index.js
@@ -0,0 +1,45 @@
+(function () {
+  const app = document.querySelector(".app");
+  const timeInput = app.querySelector(".app__input");
+  const addTimerButton = app.querySelector(".app__add-timer");
+  const timersList = app.querySelector(".app__timers");
+
+  function createElem(tag, className, parent, content) {
+    const elem = document.createElement(tag);
+    elem.className = className;
+    if (content) {
+      elem.textContent = content;
+    }
+    if (parent) {
+      parent.appendChild(elem);
+    }
+    return elem;
+  }
+
+  function createTimer(duration) {
+    console.log(duration);
+    let timeRemaining = duration;
+    const timerElement = createElem("li", "timer", timersList);
+    console.log(timerElement);
+    const timeDisplay = createElem(
+      "span",
+      "timer__time",
+      timerElement,
+      timeRemaining
+    );
+    const stopButton = createElem(
+      "button",
+      "timer__button",
+      timerElement,
+      "Удалить"
+    );
+  }
+
+  addTimerButton.addEventListener("click", () => {
+    const duration = parseInt(timeInput.value, 10);
+    if (!isNaN(duration) && duration > 0) {
+      createTimer(duration);
+      timeInput.value = "";
+    }
+  });
+})();
